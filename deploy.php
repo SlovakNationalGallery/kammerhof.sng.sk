572a54d8abca36597cb5b8dc417dded58326a0a9
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-04-10 11:47:23 +0200

    setup deploy

--- deploy.php
+++ deploy.php
@@ -5,12 +5,13 @@ require 'recipe/laravel.php';
 
 set('bin/php', 'php8.1');
 set('bin/composer', '{{bin/php}} $(which composer2)');
+set('bin/npm', 'n --offline exec 20 npm');
 
 // Project name
 set('application', 'atlas');
 
 // Project repository
-set('repository', 'git@github.com:SlovakNationalGallery/atlas.sng.sk.git');
+set('repository', 'git@github.com:SlovakNationalGallery/kammerhof.sng.sk.git');
 
 // Shared files/dirs between deploys
 add('shared_dirs', ['resources/fonts']);
@@ -18,26 +19,18 @@ add('shared_dirs', ['resources/fonts']);
 set('allow_anonymous_stats', false);
 
 // Hosts
-host('test')
-    ->set('hostname', 'webumenia.sk')
+host('kammerhof.sng.sk')
+    ->set('hostname', 'kammerhof.sng.sk')
     ->set('remote_user', 'lab_sng')
-    ->set('deploy_path', '/var/www/test.atlas.sng.sk');
-
-host('production')
-    ->set('hostname', 'webumenia.sk')
-    ->set('remote_user', 'lab_sng')
-    ->set('branch', 'master')
-    ->set('deploy_path', '/var/www/atlas.sng.sk');
+    ->set('deploy_path', '/var/www/kammerhof.sng.sk');
 
 // Tasks
 task('build', function () {
     cd('{{release_path}}');
-
-    run('source "$HOME/.nvm/nvm.sh" && nvm use 16.16.0 && npm ci && npm run build');
+    run('{{bin/npm}} ci');
+    run('{{bin/npm}} run build');
 });
 
 // Hooks
-before('artisan:migrate', 'artisan:cache:clear');
 after('deploy:vendors', 'build');
-after('deploy:symlink', 'artisan:queue:restart');
 after('deploy:failed', 'deploy:unlock');
