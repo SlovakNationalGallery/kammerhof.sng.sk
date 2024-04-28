a7be30eb0b59153c08d1a14166bccd28b82697b8
Igor Rjabinin <igor.rjabinin@gmail.com>
2024-04-29 00:09:07 +0200

    install + setup vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
+        VitePWA({ registerType: 'autoUpdate' }),
         vue({
             template: {
                 transformAssetUrls: {
